bb764f8bfcdb7db76dece5c965ec54f2552a931b
Bastian Krones <kronesbastian@gmail.com>
2024-06-03 23:13:04 +0200

    cleans code

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  default(): string {
+    return '✅ CODE FREEZE WORKING';
   }
 }
