7534a6afcd0dd1dd37ec423bc8677b447a64de9c
Bastian Krones <kronesbastian@gmail.com>
2024-06-03 23:40:41 +0200

    adds events and app run

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { EventEmitter2, OnEvent } from '@nestjs/event-emitter';
 import { asleep } from 'src/shared/wait.utils';
+import { APP } from './app.config';
 
 @Injectable()
 export class AppService {
+  constructor(private event: EventEmitter2) {}
+
   async onApplicationBootstrap() {
-    console.log('START');
-    await asleep();
+    console.clear();
+    this.event.emit(APP.RUN);
     console.log('END');
+  }
+
+  @OnEvent(APP.RUN)
+  async boot() {
+    let canRun = true;
+    let isDelay = true;
+    while (canRun) {
+      canRun = true;
+      isDelay = true;
+      console.log('looping');
+      try {
+        // RUNNING CODE FREEZE LOGIC
+      } catch (error: any) {
+        // HANDLE CODE FREEZE ERRORS
+        canRun = false;
+        isDelay = true;
+      }
+      await asleep(isDelay);
+    }
   }
 }
