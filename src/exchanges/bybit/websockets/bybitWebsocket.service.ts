a8258a023ca21f5c3871e986f9f635cadcd8b0af
Bastian Krones <kronesbastian@gmail.com>
2024-06-08 22:46:10 +0200

    adds 20s ping

--- src/exchanges/bybit/websockets/bybitWebsocket.service.ts
+++ src/exchanges/bybit/websockets/bybitWebsocket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { WebSocketSubject, webSocket } from 'rxjs/webSocket';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
+import { timer } from 'rxjs';
 
 interface BybitTicker {
   topic: string;
@@ -14,12 +15,16 @@ interface BybitTicker {
 
 interface BybitRequest {
   op: 'subscribe' | 'unsubscribe';
-  args: string[];
+  args?: string[];
+}
+
+interface BybitPing {
+  op: 'ping';
 }
 
 @Injectable()
 export class BybitTickerService {
-  private socket$: WebSocketSubject<BybitRequest | BybitTicker>;
+  private socket$: WebSocketSubject<BybitRequest | BybitTicker | BybitPing>;
 
   constructor() {
     this.socket$ = webSocket<BybitRequest | BybitTicker>(
@@ -33,15 +38,26 @@ export class BybitTickerService {
       args: [`tickers.${symbol}`],
     } as BybitRequest);
 
-    return this.socket$.pipe(
-      filter(
-        (message): message is BybitTicker =>
-          'topic' in message &&
-          message.topic === `tickers.${symbol}` &&
-          (message.type === 'snapshot' || message.type === 'delta') &&
-          message.data.markPrice != null,
-      ), // Accept both snapshot and delta
-      map((message) => message.data),
+    const ping$ = timer(0, 20000).pipe(
+      // Ping every 20 seconds
+      tap(() => {
+        this.socket$.next({ op: 'ping' } as BybitPing);
+      }),
+    );
+
+    return ping$.pipe(
+      switchMap(() =>
+        this.socket$.pipe(
+          filter(
+            (message): message is BybitTicker =>
+              'topic' in message &&
+              message.topic === `tickers.${symbol}` &&
+              (message.type === 'snapshot' || message.type === 'delta') &&
+              message.data.markPrice != null,
+          ), // Accept both snapshot and delta
+          map((message) => message.data),
+        ),
+      ),
     );
   }
 
